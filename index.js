cb2450194161ad47dff7344009b655d2c9a43aec
Dejanee Gomez <dejaneegomez@gmail.com>
2024-01-11 10:46:03 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
-// Write your code here
+let multiply = 62;
+let num1 = 31;
+let num2 = 2;
+function multiply(num1, num2) {
+  return num1 * num2;
+  //=> 62
+}
+let random = 5;
+function random() {
+  return Number.isInteger(5);
+  //=> true
+}
+let mod = 4;
+let num3 = 17;
+let num4 = 13;
+function mod (num3, num4) {
+  return num3 % num4;
+  //=> 4
+}
+let max = 20;
+function max() {
+  return Math.max(12, 14, 16, 18, 20);
+  //=> 20
+}
